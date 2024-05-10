b97efe4306e0fef7bbe82973fb58b569569ac06d
Corto07 <claudiocarruz@gmail.com>
2024-05-10 16:31:34 -0300

    Init Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// App.js
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Contacto from "./components/Contacto";
+import { Service } from "./components/Service";
+import { AuthProvider } from "./context/AuthContext";
+import UserLogin from "./components/UserLogin";
+import UserRegistration from "./components/UserRegistration";
+import NavBar from "./components/NavBar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+  const [isRegistrationModalOpen, setIsRegistrationModalOpen] = useState(false);
+
+  const openLoginModal = () => {
+    console.log("Abriendo modal de inicio de sesión");
+    setIsLoginModalOpen(true);
+  };
+
+  const closeLoginModal = () => {
+    setIsLoginModalOpen(false);
+  };
+
+  const openRegistrationModal = () => {
+    console.log("Abriendo modal de registro de usuario");
+    setIsRegistrationModalOpen(true);
+  };
+
+  const closeRegistrationModal = () => {
+    setIsRegistrationModalOpen(false);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <AuthProvider>
+      <div className="App">
+        <NavBar
+          openLoginModal={openLoginModal}
+          openRegistrationModal={openRegistrationModal}
+        />
+        <Routes>
+          <Route
+            path="/login"
+            element={
+              <UserLogin
+                isOpenL={isLoginModalOpen}
+                onCloseL={closeLoginModal}
+              />
+            }
+          />
+          <Route
+            path="/registration"
+            element={
+              <UserRegistration
+                isOpen={isRegistrationModalOpen}
+                onClose={closeRegistrationModal}
+              />
+            }
+          />
+          <Route exact path="/" element={<Home />} />
+          <Route path="/contacto" element={<Contacto />} />
+          <Route path="/service" element={<Service />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </AuthProvider>
+  );
 }
 
-export default App
+export default {App, openLoginModal, openRegistrationModal} 
